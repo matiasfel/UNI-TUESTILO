0472cb05a898f325f79e334ef65cb8f3d8547ea3
Matías Baxman <matiasbaxman@gmail.com>
2024-06-26 15:25:23 -0400

    Update all

--- tuestilo/urls.py
+++ tuestilo/urls.py
@@ -2,13 +2,11 @@ from django.urls import path
 from  . import views
 
 urlpatterns = [
+    path('login', views.login, name='login'),
     path(''        , views.base,     name = 'base'     ),
     path('info'    , views.info,     name = 'info'     ),
     path('catalogo', views.catalogo, name = 'catalogo' ),
     path('contacto', views.contacto, name = 'contacto' ),
     path('carro'   , views.carro,    name = 'carro'    ),
-    path('register', views.register, name = 'register' )
-
-
-    
+    path('register', views.register, name = 'register' ),
 ]
